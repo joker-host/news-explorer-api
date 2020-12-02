1a99eb5c8cbe03bdad70e42f43cfc1cc5bdbec70
Andrey <joker-host@yandex.ru>
2020-12-02 22:22:45 +0300

    Исправлен контроллер удаления статей

--- controllers/article.js
+++ controllers/article.js
@@ -6,7 +6,7 @@ const UnauthorizedError = require('../errors/UnauthorizedError');
 const getArticlesById = (req, res, next) => {
   Article.find({ owner: req.user.id })
     .orFail(() => {
-      send([])
+      res.status(200).send([])
     })
     .then((data) => res.status(200).send(data))
     .catch(next);
